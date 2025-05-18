ccedd0a5bbeba57975712fb817bd72708fb29b5d
EltonVitorettiJr <eltonvitorettijr@gmail.com>
2025-05-18 18:08:05 -0300

    terminado lógica dos botões de interacao do site

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const showAllButton = document.querySelector(`.show-all-button`)
 const discountButton = document.querySelector(`.discount-button`) 
+const totalButton = document.querySelector(`.total-price-button`)
+const veganButton = document.querySelector(`.vegan-button`)
 const listMenu = document.querySelector(`ul`)
+
+let discountON = false
 
 function showAll (listType) {
     
     let list = ``
-    
+    discountON = false
+
     listType.forEach(burger => {
         
         list += 
@@ -21,7 +26,7 @@ function showAll (listType) {
 }
 
 function discount() {
-    
+
     let discountList = menuOptions.map(burger => ({
         ...burger,
         price: burger.price * 0.9
@@ -29,7 +34,47 @@ function discount() {
 
     showAll(discountList)
 
+    discountON = true
+}
+
+function totalPrice() {
+
+    let totalPrice = 0
+
+    
+    if (discountON === false) {
+        
+        totalPrice = menuOptions.reduce((acc, burger) => {
+            return acc += burger.price
+        }, 0)
+
+        listMenu.innerHTML = 
+        `<li>
+            <p>The total price of burgers is: </p>
+            <p class="price-burger">R$: ${totalPrice.toFixed(2)}</p>
+        </li>`
+
+    } else {
+        
+        totalPrice = menuOptions.reduce((acc, burger) => {
+            return acc += burger.price * 0.9
+        }, 0)
+
+        listMenu.innerHTML = 
+        `<li>
+            <p>The total price of burgers is: </p>
+            <p class="price-burger">R$: ${totalPrice.toFixed(2)}</p>
+        </li>`
+
+    }
+
+}
+
+function veganFilter() {
+    showAll(menuOptions.filter(burger => burger.vegan))
 }
 
 showAllButton.addEventListener(`click`, () => showAll(menuOptions))
 discountButton.addEventListener(`click`, discount)
+totalButton.addEventListener(`click`, () => totalPrice())
+veganButton.addEventListener(`click`, veganFilter)
